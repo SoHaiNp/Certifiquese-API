a0e150c92e0d9d3f010ce060d7d95e77edb95531
SoHaiNp <139929891+SoHaiNp@users.noreply.github.com>
2024-03-26 00:52:54 -0300

    Commit inicial

---
+++ src/main/java/com/vale/certifiqueseapi/CertifiqueseApiApplication.java
@@ -0,0 +1,13 @@
+package com.vale.certifiqueseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifiqueseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifiqueseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vale/certifiqueseapi/CertifiqueseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vale.certifiqueseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifiqueseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
